b1a273ce1d98034f5891a8a67e1f600913c6e1f9
ilyashq <ilyasenaa24@gmail.com>
2024-07-26 11:00:58 +0100

    ajouter la fonctionnalité de création de réservation pour un événement spécifique

--- HelloEvents/src/main/java/com/Events/HelloEvents/controller/ContactController.java
+++ HelloEvents/src/main/java/com/Events/HelloEvents/controller/ContactController.java
@@ -1,4 +1,26 @@
-package com.Events.HelloEvents.controller;
-
-public class ContactController {
-}
+//package com.Events.HelloEvents.controller;
+//
+//import com.Events.HelloEvents.dto.ContactDTO;
+//import com.Events.HelloEvents.service.ContactService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/contacts")
+//public class ContactController {
+//
+//    @Autowired
+//    private ContactService contactService;
+//
+//    @PostMapping
+//    public ResponseEntity<ContactDTO> createContact(@RequestBody ContactDTO contactDTO) {
+//        if (contactDTO == null || contactDTO.getUser() == null) {
+//            return ResponseEntity.badRequest().body(null);
+//        }
+//
+//        ContactDTO createdContactDTO = contactService.createContact(contactDTO);
+//        return ResponseEntity.ok(createdContactDTO);
+//    }
+//}
+//
